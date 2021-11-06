3391dc405955a14ba3b5e9708776eafcc77b4082
Artem Maksymov <armaksimov3@gmail.com>
2021-11-05 21:07:31 -0400

    Project files created and filled with minimal functionality.
    Rock_paper_scissors and Rock_paper_scissors.Tasts are created and Rock_paper_scissors has main.c with minimal functionality.

---
+++ CSCN71020_Assign3/Rock_paper_scissors.Tests/Rock_paper_scissors.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RockpaperscissorsTests
+{
+	TEST_CLASS(RockpaperscissorsTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ CSCN71020_Assign3/Rock_paper_scissors/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* rockPaperScissors(char player1_choice[], char player2_choice[]);
+
+int main() {
+	return 0;
+}
+
+char* rockPaperScissors(char player1_choice[], char player2_choice[]) {
+	char result[10];
+	return result;
+}
