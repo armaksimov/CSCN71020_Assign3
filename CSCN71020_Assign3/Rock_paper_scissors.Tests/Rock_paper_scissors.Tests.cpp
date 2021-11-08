f32a41c415528a7786ff51a24ea79aabeb8e4d5a
Artem Maksymov <armaksimov3@gmail.com>
2021-11-07 20:11:46 -0500

    Functionality for main.c added.
    Function rockPaperScissors can now conduct it`s task and passes all the tests which were previously written.
    New test in Rock_paper_scissors.Tests added in order to test functionality better.

--- CSCN71020_Assign3/Rock_paper_scissors.Tests/Rock_paper_scissors.Tests.cpp
+++ CSCN71020_Assign3/Rock_paper_scissors.Tests/Rock_paper_scissors.Tests.cpp
@@ -17,31 +17,31 @@ namespace RockpaperscissorsTests
 		{
 			char input1[10] = "rock";
 			char input2[10] = "rock";
-			char *correctResult = "Draw";
+			char *correctResult = "DRAW";
 
 			char *result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T002_rockPaperScissors_paperAndpaper)
 		{
 			char input1[10] = "paper";
 			char input2[10] = "paper";
-			char *correctResult = "Draw";
+			char *correctResult = "DRAW";
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T003_rockPaperScissors_scissorsAndscissors)
 		{
 			char input1[10] = "scissors";
 			char input2[10] = "scissors";
-			char *correctResult = "Draw";
+			char *correctResult = "DRAW";
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T004_rockPaperScissors_rockAndpaper)
 		{
@@ -51,7 +51,7 @@ namespace RockpaperscissorsTests
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T005_rockPaperScissors_rockAndscissors)
 		{
@@ -61,7 +61,7 @@ namespace RockpaperscissorsTests
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T006_rockPaperScissors_paperAndscissors)
 		{
@@ -71,37 +71,37 @@ namespace RockpaperscissorsTests
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T007_rockPaperScissors_rockAndnothing)
 		{
 			char input1[10] = "rock";
 			char input2[10] = "nothing";
-			char* correctResult = "Invalid";
+			char* correctResult = "INVALID";
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T008_rockPaperScissors_rockAndrock)
 		{
 			char input1[10] = "rock";
 			char input2[10] = "";
-			char* correctResult = "Invalid";
+			char* correctResult = "INVALID";
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T009_rockPaperScissors_ROckAndroCK)
 		{
 			char input1[10] = "ROck";
 			char input2[10] = "roCK";
-			char* correctResult = "Draw";
+			char* correctResult = "DRAW";
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
 		}
 		TEST_METHOD(T010_rockPaperScissors_sCiSsOrS_Androck)
 		{
@@ -111,7 +111,17 @@ namespace RockpaperscissorsTests
 
 			char* result = rockPaperScissors(input1, input2);
 
-			Assert::AreEqual(result, correctResult);
+			Assert::AreEqual(correctResult, result);
+		}
+		TEST_METHOD(T011_rockPaperScissors_nullAndnull)
+		{
+			char input1[10] = "";
+			char input2[10] = "";
+			char* correctResult = "INVALID";
+
+			char* result = rockPaperScissors(input1, input2);
+
+			Assert::AreEqual(correctResult, result);
 		}
 	};
 }
--- CSCN71020_Assign3/Rock_paper_scissors/main.c
+++ CSCN71020_Assign3/Rock_paper_scissors/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>  
 #include "main.h"
+
+#define DRAW "DRAW"
+#define INVALID "INVALID"
+#define PLAYER1 "Player1"
+#define PLAYER2 "Player2"
+#define ROCK "rock"
+#define PAPER "paper"
+#define SCISSORS "scissors"
 
 char* rockPaperScissors(char player1_choice[], char player2_choice[]);
 
@@ -9,6 +18,34 @@ int main() {
 }
 
 char* rockPaperScissors(char player1_choice[], char player2_choice[]) {
-	char result[10];
-	return result;
+	if (player1_choice[0] == NULL || player2_choice[0] == NULL) {
+		return "INVALID";
+	}
+
+	int i;
+	int lim1 = sizeof(player1_choice) / sizeof(player1_choice[0]);
+	int lim2 = sizeof(player2_choice) / sizeof(player2_choice[0]);
+	for (i = 0; i < lim1; i++) {
+		char temp = tolower(player1_choice[i]);
+		player1_choice[i] = temp;
+	}
+	for (i = 0; i < lim2; i++) {
+		char temp = tolower(player2_choice[i]);
+		player2_choice[i] = temp;
+	}
+
+	if (strcmp(player1_choice, player2_choice) == 0) {
+		return DRAW;
+	}
+	
+	else if ((strcmp(player1_choice, ROCK) == 0 && strcmp(player2_choice, PAPER) == 0) || (strcmp(player1_choice, PAPER) == 0 && strcmp(player2_choice, SCISSORS) == 0) || (strcmp(player1_choice, SCISSORS) == 0 && strcmp(player2_choice, ROCK) == 0)) {
+		return PLAYER2;
+	}
+	else if ((strcmp(player1_choice, PAPER) == 0 && strcmp(player2_choice, ROCK) == 0) || (strcmp(player1_choice, SCISSORS) == 0 && strcmp(player2_choice, PAPER) == 0) || (strcmp(player1_choice, ROCK) == 0 && strcmp(player2_choice, SCISSORS) == 0)) {
+		return PLAYER1;
+	}
+	else {
+		return INVALID;
+	}
 }
+
